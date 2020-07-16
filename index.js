3b5e3c396ca5637197bf7fc43d91df4b7bf33abc
mcgillshakore <mcgills2093@gmail.com>
2020-07-15 20:44:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(name, event_name) {
+
+    let result = [];
+
+    for(let i = 0; i < name.length; i++) {
+        result.push(`Thank you, ${name[i]}, for the wonderful ${event_name} gift!`)
+    }
+
+    return result;
+}
+
+function countDown(integer) {
+    let counter = 0;
+    while (counter <= 10) {
+        console.log(counter);
+
+        counter++;
+    }
+}
